37a9947ed716c5ad80cbf9cef37926f67134a862
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-11-19 13:07:13 -0500

    Added input.js

--- play.js
+++ play.js
@@ -1,28 +1,6 @@
 const { connect } = require('./client');
-
+const { setupInput } = require('./input')
 const net = require("net");
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === "\u0003") {
-    return process.exit();
-  } else if (MOVEMENT_KEYS.hasOwnProperty(key)) {
-    return MOVEMENT_KEYS[key];
-  } else {
-    return "";
-  }
-};
-
-module.exports = { setupInput };
-
 console.log("Connecting ...");
 connect();
-
 module.exports = { connect };
