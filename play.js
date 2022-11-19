eb5accf47a11105d86ed10bdfcc433d11daae2ac
Somaro <106792137+SomarGuy@users.noreply.github.com>
2022-11-19 13:00:38 -0500

    Function setupInput & handleUserInput

--- play.js
+++ play.js
@@ -2,6 +2,26 @@ const { connect } = require('./client');
 
 const net = require("net");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === "\u0003") {
+    return process.exit();
+  } else if (MOVEMENT_KEYS.hasOwnProperty(key)) {
+    return MOVEMENT_KEYS[key];
+  } else {
+    return "";
+  }
+};
+
+module.exports = { setupInput };
+
 console.log("Connecting ...");
 connect();
 
